837796f4dfaf4229d37fefa4b9f2bb879f54985f
Jesbus <jessebusman@gmail.com>
2015-11-13 22:00:55 +0100

    Test commit from eclipse

--- src/Net/server.java
+++ src/Net/server.java
@@ -2,12 +2,16 @@ package Net;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class server {
 	private ServerSocket thissock=null;
 	
 	public server() throws IOException{
 		thissock= new ServerSocket(2656);
-		
+		while(true)
+		{
+			Socket s = thissock.accept();
+		}
 	}
 }
